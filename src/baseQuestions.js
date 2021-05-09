31247f6d1a0a823260fb2053b7330f0021d2bc79
Lee Farnell <leefarnell@mac.com>
2021-05-09 14:21:55 +0100

    created file for base questions

---
+++ src/baseQuestions.js
@@ -0,0 +1,19 @@
+const baseQuestions = [
+  {
+    type: "input",
+    message: "Please enter the employees full name?",
+    name: "name",
+  },
+  {
+    type: "number",
+    message: "What is the employees ID number?",
+    name: "employeeID",
+  },
+  {
+    type: "input",
+    message: "What is the employees email address?",
+    name: "email",
+  },
+];
+
+module.exports = baseQuestions;
