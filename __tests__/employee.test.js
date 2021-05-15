9be84f6bc35950472a88703e44a447e91e0529c6
Lee Farnell <leefarnell@mac.com>
2021-05-15 20:10:32 +0100

    added method tests to employee and engineer

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -5,6 +5,7 @@ describe("Employee Class", () => {
     name: "Lee",
     employeeID: "812",
     email: "leefarnell@mac.com",
+    role: "Employee",
   };
 
   describe("Constructor Test", () => {
@@ -17,9 +18,44 @@ describe("Employee Class", () => {
     it("should set the all the employee details inside the object ", () => {
       const actual = new Employee(mockAnswer);
 
-      expect(actual.name).toEqual("Lee"),
-        expect(actual.employeeID).toEqual("812"),
-        expect(actual.email).toEqual("leefarnell@mac.com");
+      expect(actual.name).toEqual(mockAnswer.name),
+        expect(actual.employeeID).toEqual(mockAnswer.employeeID),
+        expect(actual.email).toEqual(mockAnswer.email),
+        expect(actual.getRole()).toEqual(mockAnswer.role);
+    });
+  });
+
+  describe("Employee Methods", () => {
+    it("should return the employees name", () => {
+      const actual = new Employee(mockAnswer);
+
+      const result = actual.getName();
+
+      expect(result).toEqual(mockAnswer.name);
+    });
+
+    it("should return the employees ID number", () => {
+      const actual = new Employee(mockAnswer);
+
+      const result = actual.getId();
+
+      expect(result).toEqual(mockAnswer.employeeID);
+    });
+
+    it("should return the employees email address", () => {
+      const actual = new Employee(mockAnswer);
+
+      const result = actual.getEmail();
+
+      expect(result).toEqual(mockAnswer.email);
+    });
+
+    it("should return the employees role", () => {
+      const actual = new Employee(mockAnswer);
+
+      const result = actual.getRole();
+
+      expect(result).toEqual(mockAnswer.role);
     });
   });
 });
--- __tests__/engineer.test.js
+++ __tests__/engineer.test.js
@@ -6,6 +6,7 @@ describe("Engineer Class", () => {
     employeeID: "812",
     email: "leefarnell@mac.com",
     github: "http://www.github.com/LeeFarnell",
+    role: "Engineer",
   };
 
   describe("Constructor Test", () => {
@@ -15,13 +16,56 @@ describe("Engineer Class", () => {
       expect(actual).toBeInstanceOf(Engineer);
     });
 
-    it("should set the all the employee details inside the object ", () => {
+    it("should set the all the Engineer details inside the object ", () => {
       const actual = new Engineer(mockAnswer);
 
-      expect(actual.name).toEqual("Lee"),
-        expect(actual.employeeID).toEqual("812"),
-        expect(actual.email).toEqual("leefarnell@mac.com");
-      expect(actual.github).toEqual("http://www.github.com/LeeFarnell");
+      expect(actual.name).toEqual(mockAnswer.name),
+        expect(actual.employeeID).toEqual(mockAnswer.employeeID),
+        expect(actual.email).toEqual(mockAnswer.email),
+        expect(actual.github).toEqual("http://www.github.com/LeeFarnell");
+      expect(actual.getRole()).toEqual(mockAnswer.role);
+    });
+  });
+
+  describe("Engineer Methods", () => {
+    it("should return the engineers name", () => {
+      const actual = new Engineer(mockAnswer);
+
+      const result = actual.getName();
+
+      expect(result).toEqual(mockAnswer.name);
+    });
+
+    it("should return the engineers ID number", () => {
+      const actual = new Engineer(mockAnswer);
+
+      const result = actual.getId();
+
+      expect(result).toEqual(mockAnswer.employeeID);
+    });
+
+    it("should return the engineers email address", () => {
+      const actual = new Engineer(mockAnswer);
+
+      const result = actual.getEmail();
+
+      expect(result).toEqual(mockAnswer.email);
+    });
+
+    it("should return the engineers Github username", () => {
+      const actual = new Engineer(mockAnswer);
+
+      const result = actual.getGithub();
+
+      expect(result).toEqual(mockAnswer.github);
+    });
+
+    it("should return the engineers role", () => {
+      const actual = new Engineer(mockAnswer);
+
+      const result = actual.getRole();
+
+      expect(result).toEqual(mockAnswer.role);
     });
   });
 });
